e6221bcbe5a671bfc0abd3cc351b4a9c239c8046
Gil Teixeira <gilteixeira@ua.pt>
2021-10-20 12:21:53 +0000

    fixed context with k order

--- lab1/fcm.py
+++ lab1/fcm.py
@@ -1,4 +1,7 @@
 
+import copy
+from os import confstr
+
 class FCM:
     def __init__(self, k, a, textFile='example.txt'):
         self.k = k
@@ -6,34 +9,54 @@ class FCM:
         self.textFile = open(textFile, 'r')
         self.text = self.textFile.read()
         self.createContext()
+        self.probabilitiesContext = self.calculateProbabilities(self.context)
+
+    def calculateProbabilities(self, context, k=0):
+        res = copy.deepcopy(context) 
+        total_probability = 0
+        for char in res.keys():
+            if isinstance(res[char], int)  or isinstance(res[char], float):
+                total_probability += res[char]
+        for char in res.keys():
+            if isinstance(res[char], int)  or isinstance(res[char], float):
+                res[char] = res[char] / (total_probability)
+            elif isinstance(res[char], dict):
+                if k < self.k:
+                    res[char] = self.calculateProbabilities(res[char], k+1)
+            else:
+                print("not a number nor a dict: " + res[char])
+        return res
+            
+
 
     def createContext(self):
         res = {}
-        trash_chars = ['', '|', '!', '"', '$', '%', '&', '/', '(', ')', '=', '?', '\'' , '»', '\\', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '{', '[', ']', '}', '«', ',', '.', ';', ':', '-', '_']
+        parent_ref = {}
+        trash_chars = ['', '\n', '|', '!', '"', '$', '%', '&', '/', '(', ')', '=', '?', '\'' , '»', '\\', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '{', '[', ']', '}', '«', ',', '.', ';', ':', '-', '_']
         for k_order in range(self.k):
             for word in self.text.split(' '):
-                if(k_order > len(word)-1):
-                    break
-                k_order_dict = {}
-                if k_order != 0:
-                    previous_state_indexes = word[0:k_order-1]
-                    tmp_dict = res
-                    for char in previous_state_indexes:
-                        if char not in trash_chars:
-                            tmp_dict = tmp_dict[char]
-                    k_order_dict = tmp_dict
-                if word[k_order] not in trash_chars:
-                    if word[k_order] not in k_order_dict.keys():
-                        k_order_dict.setdefault(word[k_order], self.a)
+                if(k_order > len(word) -1):
+                    continue
+                bad_word = False
+                for char in word:
+                    if char in trash_chars:
+                        bad_word = True
+                if bad_word:
+                    continue
+                current_ref = res
+                for i in range(k_order):
+                    current_ref = current_ref[word[i]]
+                if isinstance(current_ref, dict):
+                    if word[k_order] not in current_ref.keys():
+                        current_ref.setdefault(word[k_order], self.a)
                     else:
-                        k_order_dict[word[k_order]] += 1
-                if k_order == 0:
-                    res = k_order_dict
+                        current_ref[word[k_order]] += 1
                 else:
-                    res[previous_state_indexes] = k_order_dict
+                    res[word[:k_order]] = {word[k_order] : self.a}
+                    
         self.context = res
+
                 
 
-a = FCM(3,0.3)
-
+a = FCM(2,0.3)
     
