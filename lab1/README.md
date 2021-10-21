2aebc0f09ff0eefca57b1550c11cbc468f385d5b
Gil Teixeira <gilteixeira@ua.pt>
2021-10-21 13:17:45 +0000

    added more details to the readme

--- lab1/README.md
+++ lab1/README.md
@@ -4,4 +4,9 @@ Algorithmic Information Theory
 The simple test was made in order to verify simply nested (second order) words.
 More information about how to call fcm is presented when you run:
 python3 fcm.py
+and the same with the generator:
+python3 generator.py
+Furthermore there's a test module with some expected results for simple tasks. 
+The tests are ran after:
+Python3 testFCM.py
 ```
--- lab1/generator.py
+++ lab1/generator.py
@@ -7,44 +7,47 @@ if __name__ == "__main__":
     text_length = 200
     text = ""
     a = FCM
+    done = False
     if len(sys.argv) == 1:
         print("The program show be called like this: \n\tpython3 generator.py filename string_length ||\n\tpython3 generator.py k_order filename string_length ||\n\tpython3 generator.py k_order a filename string_length\nk_order meaning de order of the model and\na being a smoothing parameter")
-    elif len(sys.argv) == 3:
-        a = FCM(4,0.3, sys.argv[1])
-        text_length = int(sys.argv[2])
-    elif len(sys.argv) == 4:
-        a = FCM(int(sys.argv[1]),0.3, sys.argv[2])
-        text_length = int(sys.argv[3])
-    elif len(sys.argv) == 5:
-        a = FCM(int(sys.argv[1]),float(sys.argv[2]), sys.argv[3])
-        text_length = int(sys.argv[4])
-    context_pile = []
-    context_pile.append(a.probabilitiesContext)
-    print(a.context)
-    print(a.probabilitiesContext)
-    for i in range(text_length):
-        if(len(context_pile)== 0):
-            context_pile.append(a.probabilitiesContext)
-            text+=" "
-        current_context = context_pile.pop()
-        probs = []
-        for prob in current_context:
-            if isinstance(current_context[prob], int) or isinstance(current_context[prob], float):
-                probs.append(current_context[prob])
-            elif not isinstance(current_context[prob],str):
-                probs.append(FCM.countContextChildren(a, current_context[prob]))
-        keys = list(current_context.keys())
-        if 'char' in keys:
-            keys.remove('char')
-        if 'value' in keys:
-            keys.remove('value')
-            keys.append('')
-        res = random.choices(keys, probs)[0]
-        if '' in keys:
-            keys.remove('')
-        if res != '':
-            text += res
-            if not (isinstance(current_context[res], int) or isinstance(current_context[res], float)):
-                context_pile.append(current_context[res])
-    print(text)
-    
+        done = True
+    if not done:
+        if len(sys.argv) == 3:
+            a = FCM(4,0.3, sys.argv[1])
+            text_length = int(sys.argv[2])
+        elif len(sys.argv) == 4:
+            a = FCM(int(sys.argv[1]),0.3, sys.argv[2])
+            text_length = int(sys.argv[3])
+        elif len(sys.argv) == 5:
+            a = FCM(int(sys.argv[1]),float(sys.argv[2]), sys.argv[3])
+            text_length = int(sys.argv[4])
+        context_pile = []
+        context_pile.append(a.probabilitiesContext)
+        i = 0
+        while i < text_length: 
+            if(len(context_pile)== 0):
+                context_pile.append(a.probabilitiesContext)
+                text+=" "
+                i += 1
+            current_context = context_pile.pop()
+            probs = []
+            for prob in current_context:
+                if isinstance(current_context[prob], int) or isinstance(current_context[prob], float):
+                    probs.append(current_context[prob])
+                elif not isinstance(current_context[prob],str):
+                    probs.append(FCM.countContextChildren(a, current_context[prob]))
+            keys = list(current_context.keys())
+            if 'char' in keys:
+                keys.remove('char')
+            if 'value' in keys:
+                keys.remove('value')
+                keys.append('')
+            res = random.choices(keys, probs)[0]
+            if '' in keys:
+                keys.remove('')
+            if res != '':
+                text += res
+                if not (isinstance(current_context[res], int) or isinstance(current_context[res], float)):
+                    context_pile.append(current_context[res])
+        print(text)
+        
