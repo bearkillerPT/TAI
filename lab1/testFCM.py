da1276ee69dcec48c71f9ed9b8c986405c4a08cb
Gil Teixeira <gilteixeira@ua.pt>
2021-10-23 13:25:21 +0000

    added another example and restructured the tests file tree

--- lab1/testFCM.py
+++ lab1/testFCM.py
@@ -1,13 +1,17 @@
 from fcm import FCM
 
-tests= ["example1", "example2", "example3"]
+tests= ["example1", "example2", "example3", "example4"]
 
 for test in tests:
     a = FCM(3, 0.3, textFile='tests/' + test + '.txt')
-    expected_counts_file = 'tests/' + test + '.expected'
-    expected_probs_file = 'tests/' + test + 'Probs.expected'
-    expected_counts = open(expected_counts_file, 'r').read()
-    expected_probs = open(expected_probs_file, 'r').read()
+    expected_counts_file = 'tests/expected/' + test + '.expected'
+    expected_probs_file = 'tests/expected/' + test + 'Probs.expected'
+    try:
+        expected_counts = open(expected_counts_file, 'r').read()
+        expected_probs = open(expected_probs_file, 'r').read()
+    except:
+        print("Files for expected results of " + test + " are missing!")
+
     if(str(a.context) + str(a.probabilitiesContext) == expected_counts + expected_probs):
         print("Test: " + test + " Passed!")
     else:
