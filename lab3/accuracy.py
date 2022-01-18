c61e3257588f7c3ed2440ff9e6c4a9664f7c3650
Your Name <joaorelva24@gmail.com>
2022-01-18 16:12:32 -0500

    accuracy

---
+++ lab3/accuracy.py
@@ -0,0 +1,50 @@
+from random import sample
+from charizam import CHARIZAM
+import os
+
+def getAllSamples(samples_dir):
+    arr = os.listdir(samples_dir)
+    return arr
+
+def classification(sampleWav,guessed_song):
+    sampleNames = {
+        "sample01.wav":"SUICIDEBOYS - PARIS","sample02.wav":"2pac ft Dr Dre - California Love","sample03.wav":"AC DC - Highway to Hell",
+        "sample04.wav":"Big Pun - Beware","sample05.wav":"Biggie Smalls - Party and Bullshit","sample06.wav":"Bobby Shmurda - Hot N_gga",
+        "sample07.wav":"Bon Jovi - Livin On A Prayer","sample08.wav":"Chief Keef - Love Sosa","sample09.wav":"Coldplay - Viva La Vida",
+        "sample10.wav":"Deep Purple - Smoke on the Water","sample11.wav":"Dr Dre feat Snoop Dogg - Still Dre","sample12.wav":"Dr Dre ft Snoop Dogg - Deep Cover",
+        "sample13.wav":"Foo Fighters - The Pretender","sample14.wav":"Green Day - American Idiot","sample15.wav":"Gunna and Future - pushin P",
+        "sample16.wav":"Guns N Roses - Sweet Child O Mine","sample17.wav":"Ice Cube - Ghetto Bird","sample18.wav":"Kaiser Chiefs - Ruby",
+        "sample19.wav":"Led Zeppelin - Whole Lotta Love","sample20.wav":"lil peep x lil tracy - witchblades","sample21.wav":"LMFAO - Sexy and I Know It",
+        "sample22.wav":"Metallica - The Memory Remains","sample23.wav":"LON3R JOHNY - TRAPSTAR","sample24.wav":"Lynyrd Skynyrd - Sweet Home Alabama",
+        "sample25.wav":"Maroon 5 - Harder To Breathe","sample26.wav":"Michael Bublé - Feeling Good","sample27.wav":"Michael Jackson - Bad",
+        "sample28.wav":"Michael Jackson - Billie Jean","sample29.wav":"Mobb Deep - Shook Ones Part 2","sample30.wav":"Nas - NY State Of Mind",
+        "sample31.wav":"Nirvana - Smells Like Teen Spirit","sample32.wav":"Numb - Linkin Park","sample33.wav":"NWA - Fuk Da Police",
+        "sample34.wav":"Oasis - Dont Look Back In Anger","sample35.wav":"Somewhere over the Rainbow - Israel IZ Kamakawiwo ole","sample36.wav":"Pharrell Williams - Happy",
+        "sample37.wav":"ProfJam - Agua de Coco","sample38.wav":"PSY - GANGNAM STYLE","sample39.wav":"Recayd Mob - Plaqtudum",
+        "sample40.wav":"Red Hot Chili Peppers - Dani California","sample41.wav":"Robbie Williams - Angels","sample42.wav":"Robin Thicke - Blurred Lines ft TI and Pharrell",
+        "sample43.wav":"scarlxrd - HEART ATTACK","sample44.wav":"Survivor - Eye Of The Tiger","sample45.wav":"The Black Eyed Peas - Where Is The Love",
+        "sample46.wav":"The Killers - Mr. Brightside","sample47.wav":"The White Stripes - Seven Nation Army","sample48.wav":"Travis Scott - goosebumps ft Kendrick Lamar",
+        "sample49.wav":"Whitney Houston - I Will Always Love You","sample50.wav":"Ylvis - The Fox"
+    }
+    sampleWav = sampleWav[8:]
+    if sampleNames[sampleWav] == guessed_song:
+        return 1
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    
+    samples = getAllSamples('./samples')
+    samples.sort()
+    compressor = "-b"
+    correct_guesses = 0
+    
+    for i in samples:
+        sampleWav = "samples/" + i
+        object = CHARIZAM(sampleWav,compressor)
+        correct_guesses += classification(sampleWav,object.guessed_song)
+    
+    accuracy = (correct_guesses / len(samples)) * 100
+
+    print("Accuracy: " + str(accuracy) + "%") 
--- lab3/charizam.py
+++ lab3/charizam.py
@@ -1,126 +1,139 @@
 import os
-import subprocess
 import sys
 import shutil
 import gzip
+import bz2
+import lzma
+from datetime import datetime
 
+class CHARIZAM:
 
-def getDbWavs(database_dir):
-    arr = os.listdir(database_dir)
-    return arr
+    def __init__(self,sampleWav,compressor):
+        self.deleteFiles(sampleWav)
+        self.noFreqs = False
+        self.isDbFreqs()
+        self.database_dir = "./database"
+        self.wavs = self.getDbWavs(self.database_dir)
+        self.distances = {}
+        self.createFreqsFile(sampleWav,isSample=True)
+        self.createDistances(self.wavs,self.noFreqs,sampleWav,compressor)
+        self.guessed_song = min(self.distances,key=self.distances.get)  
+        self.deleteFiles(sampleWav)
 
+       
+    def createDistances(self,wavs,noFreqs,sampleWav,compressor):
+         for i in wavs:
+            if noFreqs == True:
+                self.createFreqsFile(i,isSample=False)
+            ncdBits,music= self.ncd(sampleWav,i,compressor)
+            self.distances.update({music:ncdBits})
 
-def createFreqsFolder():
-    folder = "DbFreqs"
-    parent_dir = "."
-    path = os.path.join(parent_dir, folder)
+    def isDbFreqs(self):
+        if os.path.isdir('DbFreqs') == False:
+            self.createFreqsFolder()
+            self.noFreqs = True
 
-    if os.path.isdir(path) == False:
-        os.mkdir(path)
+    def getDbWavs(self,database_dir):
+        arr = os.listdir(database_dir)
+        return arr
 
+    def createFreqsFolder(self):
+        folder = "DbFreqs"
+        parent_dir = "."
+        path = os.path.join(parent_dir, folder)
 
-def moveToFreqsFolder(filename):
-    destination = "DbFreqs"
-    shutil.move(filename, destination)
+        if os.path.isdir(path) == False:
+            os.mkdir(path)
 
+    def moveToFreqsFolder(self,filename):
+        destination = "DbFreqs"
+        shutil.move(filename, destination)
 
-def deleteFiles(foldername,wavName):
-    freqsFileName = wavName[8:-4] + ".freqs"
-
-    if os.path.isdir(foldername) == True:
-        shutil.rmtree(foldername)
-    if os.path.isfile(freqsFileName) == True:
-        os.remove(freqsFileName)
-    if os.path.isfile('concat.freqs') == True:
-        os.remove('concat.freqs')
-    if os.path.isfile('temp.gz') == True:
-        os.remove('temp.gz')
-
-
-def createFreqsFile(wavName,isSample):
-    if isSample == False:
-        freqsFileName = wavName[:-4] + ".freqs"
-        newFreqsName = freqsFileName.replace(' ','_')
-        newWavName = wavName.replace(' ','\\ ')
-        wav = "./database" + "/" + newWavName
-        args = "./GetMaxFreqs/bin/GetMaxFreqs -w " + newFreqsName + " " + wav
-        os.system(args)
-        moveToFreqsFolder(newFreqsName)
-    else:
+    def deleteFiles(self,wavName):
         freqsFileName = wavName[8:-4] + ".freqs"
-        args = "./GetMaxFreqs/bin/GetMaxFreqs -w " + freqsFileName + " " + wavName
-        os.system(args)
 
+        if os.path.isfile(freqsFileName) == True:
+            os.remove(freqsFileName)
+        if os.path.isfile('concat.freqs') == True:
+            os.remove('concat.freqs')
+        if os.path.isfile('temp.gz') == True:
+            os.remove('temp.gz')
 
-def gzipcomp(file):
-    comp_file = "temp.gz"
-    with open(file, 'rb') as f_in:
-        with gzip.open(comp_file, 'wb') as f_out:
-            shutil.copyfileobj(f_in, f_out)
-    
-    size = os.path.getsize(comp_file)
-    return size
+    def createFreqsFile(self,wavName,isSample):
+        if isSample == False:
+            freqsFileName = wavName[:-4] + ".freqs"
+            newFreqsName = freqsFileName.replace(' ','_')
+            newWavName = wavName.replace(' ','\\ ')
+            wav = "./database" + "/" + newWavName
+            args = "./GetMaxFreqs/bin/GetMaxFreqs -w " + newFreqsName + " " + wav
+            os.system(args)
+            self.moveToFreqsFolder(newFreqsName)
+        else:
+            freqsFileName = wavName[8:-4] + ".freqs"
+            args = "./GetMaxFreqs/bin/GetMaxFreqs -w " + freqsFileName + " " + wavName
+            os.system(args)
 
+    def compressing(self,file,compressor):
+        comp_file = "temp.gz"
 
-def concatFiles(sampleFile,musicFile):
-    command = "cat " + sampleFile + " " + musicFile + " > concat.freqs"
-    os.system(command)
+        with open(file, 'rb') as f_in:
+            if compressor == '-g':
+                with gzip.open(comp_file, 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+            elif compressor == '-b': 
+                with bz2.open(comp_file, 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+            elif compressor == '-l':
+                with lzma.open(comp_file, 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
 
+        size = os.path.getsize(comp_file)
+        
+        return size
 
-def ncd(sampleFile,musicFile):
-    sampleFile = sampleFile[8:-4] + ".freqs"
+    def concatFiles(self,sampleFile,musicFile):
+        command = "cat " + sampleFile + " " + musicFile + " > concat.freqs"
+        os.system(command)
 
-    music = musicFile
-    music = music[:-4]
-
-    musicFile = musicFile[:-4] + ".freqs"
-    musicFile = musicFile.replace(' ','_')
-    musicFile = "./DbFreqs" + "/" + musicFile
-
-    concatFiles(sampleFile,musicFile)
-    concatBits = gzipcomp('concat.freqs')
-
-    sampleBits = gzipcomp(sampleFile)
-    musicBits = gzipcomp(musicFile)
-    bits = [sampleBits,musicBits]
-
-    ncd = (concatBits - min(bits))/max(bits)
-    
-    return ncd,music
-
+    def ncd(self,sampleFile,musicFile,compressor):
+        sampleFile = sampleFile[8:-4] + ".freqs"
+        music = musicFile
+        music = music[:-4]
+        musicFile = musicFile[:-4] + ".freqs"
+        musicFile = musicFile.replace(' ','_')
+        musicFile = "./DbFreqs" + "/" + musicFile
+        
+        self.concatFiles(sampleFile,musicFile)
+        
+        concatBits = self.compressing('concat.freqs',compressor)
+        sampleBits = self.compressing(sampleFile,compressor)
+        musicBits = self.compressing(musicFile,compressor)
+        
+        bits = [sampleBits,musicBits]
+        ncd = (concatBits - min(bits))/max(bits)
+        
+        return ncd,music
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 3:
+        start = datetime.now()
+        
         sampleWav = sys.argv[1]
-        
-        deleteFiles('DbFreqs',sampleWav)
-        createFreqsFolder()
-        
-        database_dir = "./database"
-        sample_dir = "./samples"
-        wavs = getDbWavs(database_dir)
-        
-        distances = {}
-
-        createFreqsFile(sampleWav,isSample=True)
+        compressor = sys.argv[2]
         
         print('Analyzing the sample...')
-
-        for i in wavs:
-            createFreqsFile(i,isSample=False)
-            ncdBits,music= ncd(sampleWav,i)
-            distances.update({music:ncdBits})
         
+        object = CHARIZAM(sampleWav,compressor)
+
         print('\n')
         print("-------------------------GUESSED SONG-------------------------")
         print("--------------------------------------------------------------")
-
-        print(min(distances,key=distances.get))
+        print(object.guessed_song)
 
         print("--------------------------------------------------------------")
-        
-        deleteFiles('DbFreqs',sampleWav)
+        print('\n')
+        print('Execution time: ' + str(datetime.now() - start))
     
     else:
         print("The program show be called like this: \n\tpython3 charizam.py sample.wav")
